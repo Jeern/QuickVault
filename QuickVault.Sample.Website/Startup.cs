acf2d45a08beec3123c83ee9a4738c85ff031173
Jesper Niedermann <jesper@niedermann.dk>
2020-06-07 20:30:48 +0200

    The package needed to use startup class

---
+++ QuickVault.Sample.Website/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using Microsoft.Owin;
+using Owin;
+using QuickVault.Configuration;
+
+[assembly: OwinStartup(typeof(QuickVault.Sample.Website.Startup))]
+
+namespace QuickVault.Sample.Website
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            QuickVaultConfigurationManager.SetRootFolder(HttpRuntime.AppDomainAppPath);
+        }
+    }
+}
