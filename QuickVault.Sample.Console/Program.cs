63eac88dfc1c3ff012ea5ff7ead2e76000c10222
Jesper Niedermann <jesper@niedermann.dk>
2020-06-07 22:36:24 +0200

    Console sample

--- QuickVault.Sample.Console/Program.cs
+++ QuickVault.Sample.Console/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using QuickVault.Configuration;
 
-namespace QuickVault.Sample.Console
+namespace QuickVault.Sample.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Reading AppSettings");
+                Console.WriteLine("DemoProp1: {0}", QuickVaultConfigurationManager.AppSettings["DemoProp1"]);
+                Console.WriteLine("DemoProp2: {0}", QuickVaultConfigurationManager.AppSettings["DemoProp2"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
