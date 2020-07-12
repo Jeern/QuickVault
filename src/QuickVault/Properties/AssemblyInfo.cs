2ba080d4347cf4ece3b1832e80c8efb4e7ebd38c
Jesper Niedermann <jesper@niedermann.dk>
2020-07-12 20:56:34 +0200

    Trying to add Nuget info

--- src/QuickVault/Properties/AssemblyInfo.cs
+++ src/QuickVault/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("QuickVault")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic QuickVault package with core functionality")]
 [assembly: AssemblyConfiguration("")]
 [assembly: Guid("5DA36E3F-AD8A-4290-8427-4108C7760718")]
