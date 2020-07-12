57c65a8651c67a9a69518c34c6aa22845eb59316
Jesper Niedermann <jesper@niedermann.dk>
2020-07-12 21:42:45 +0200

    Publishing Configuration.Core as well

--- src/QuickVault.Configuration.Core/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("QuickVault.Configuration.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: Guid("876FA151-2F91-472E-847B-670BF15C503F")]
