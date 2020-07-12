f9b5dba5d760dec575b815cffe8d89377c911be2
Jesper Niedermann <jesper@niedermann.dk>
2020-07-12 21:00:34 +0200

    Using csproj files version info perhaps it works better

--- src/QuickVault/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("QuickVault")]
-[assembly: AssemblyDescription("Basic QuickVault package with core functionality")]
-[assembly: AssemblyConfiguration("")]
-[assembly: Guid("5DA36E3F-AD8A-4290-8427-4108C7760718")]
