4c272272f62ade39f1e270fd137981358ba50419
Jesper Niedermann <jesper@niedermann.dk>
2020-07-12 22:08:45 +0200

    Trying some a new build step that uses msbuild to see if it works for full framework

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyVersion("0.9.4.0")]
--- src/QuickVault.Configuration/Properties/AssemblyInfo.cs
+++ src/QuickVault.Configuration/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("QuickVault.Configuration")]
-[assembly: AssemblyDescription("Configuration of the QuickVault for the .NET Full framework")]
+[assembly: AssemblyDescription("QuickVault Configuration functionality")]
 [assembly: AssemblyConfiguration("")]
 [assembly: Guid("4f39f8a4-4d84-4aa3-885d-3c3134a3549d")]
